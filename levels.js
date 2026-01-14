8dbf3b929a86062c92307536093beb318af13c73
I-did <b92mvwsxm8@privaterelay.appleid.com>
2026-01-14 11:27:37 +0100

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,47 @@
+const QUEENS_LEVELS = [
+    {
+        id: 101,
+        colorRegions: [
+            ["E", "E", "E", "E", "E", "E", "F"],
+            ["D", "D", "D", "D", "D", "E", "F"],
+            ["D", "D", "D", "D", "D", "E", "F"],
+            ["E", "E", "E", "E", "G", "G", "F"],
+            ["C", "C", "C", "C", "G", "G", "F"],
+            ["A", "B", "B", "C", "G", "G", "F"],
+            ["A", "A", "B", "C", "G", "G", "F"]
+        ],
+        regionColors: {
+            A: "#d4bbff", B: "#ffcc80", C: "#99f5ff", D: "#b2eeb2", E: "#d1d5db", F: "#ff9999", G: "#f3e099"
+        }
+    },
+    {
+        id: 169,
+        colorRegions: [
+            ["D", "D", "D", "D", "D", "D", "G"],
+            ["A", "A", "D", "C", "G", "G", "G"],
+            ["G", "B", "B", "C", "C", "G", "G"],
+            ["G", "G", "G", "C", "C", "G", "G"],
+            ["E", "E", "G", "F", "F", "G", "G"],
+            ["E", "E", "F", "F", "F", "G", "G"],
+            ["G", "G", "G", "G", "G", "G", "G"]
+        ],
+        regionColors: {
+            A: "#d4bbff", B: "#ffcc80", C: "#99f5ff", D: "#b2eeb2", E: "#ff99ff", F: "#ff9999", G: "#e2f399"
+        }
+    },
+    {
+        id: 196,
+        colorRegions: [
+            ["A", "A", "B", "B", "B", "C", "C"],
+            ["A", "A", "A", "B", "B", "C", "C"],
+            ["D", "D", "A", "D", "D", "E", "C"],
+            ["D", "D", "D", "D", "D", "E", "E"],
+            ["F", "D", "D", "D", "G", "G", "E"],
+            ["F", "D", "D", "D", "D", "G", "E"],
+            ["F", "F", "F", "F", "F", "G", "E"]
+        ],
+        regionColors: {
+            A: "#d4bbff", B: "#ffcc80", C: "#99f5ff", D: "#b2eeb2", E: "#d1d5db", F: "#ff9999", G: "#f3e099"
+        }
+    }
+];
